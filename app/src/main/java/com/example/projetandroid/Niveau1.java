8ea9276c9d6b440c60878e91d8068feee3b50307
Megadreams <valendoffj@gmail.com>
2024-03-18 16:09:27 +0100

    Creation de la méthode pour verifier deux collision, utiliser pour rendre la ball visible ne marche que dans le sens pour rendre invisible je ne comprend pas pourquoi

--- app/src/main/java/com/example/projetandroid/Niveau1.java
+++ app/src/main/java/com/example/projetandroid/Niveau1.java
@@ -3,7 +3,10 @@ package com.example.projetandroid;
 import static com.example.projetandroid.Difficultes.DIFFICULTE;
 import static com.example.projetandroid.Difficultes.NIVEAU;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,6 +21,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import android.view.MotionEvent;
+import android.util.Log;
+
 
 
 
@@ -29,14 +34,34 @@ public class Niveau1 extends AppCompatActivity {
 
     private float x, y; // Variables pour stocker les coordonnées de la balle
     private ImageView ball; // Renommer l'imageView en "ball"
-
-    private ImageView bucket;
-
-
+    private ImageView hoopfront;
+    private View bucket;
 
 
+
+    private PointF hoop_l;
+    private PointF hoop_r;
+
+    private PointF ball_t;
+    private PointF ball_b;
+
+    private boolean top_check = false;
+    private boolean bottom_check = false ;
+
+    private float hoop_y;
+
+    private float ball_radius;
+
+
+
+
+
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_niveau1);
@@ -53,10 +78,30 @@ public class Niveau1 extends AppCompatActivity {
 
         // Ball
         ball = findViewById(R.id.ball);
-        ball.setVisibility(View.INVISIBLE);
+        ball_radius = (float) ball.getHeight() / 2;
+        ball.setVisibility(View.VISIBLE);
+
+        ball_t = new PointF(ball.getX(), ball.getY() - ball_radius);
+        ball_b = new PointF(ball.getX(), ball.getY() + ball_radius);
+
+
+
+
+
+        hoopfront = findViewById(R.id.hoopfront);
+
+        hoop_y = Math.round(hoopfront.getY()) - 62;
+
+        hoop_l = new PointF(Math.round(hoopfront.getX()) - 63, hoop_y);
+        hoop_r = new PointF(Math.round(hoopfront.getX()) + 63, hoop_y);
+
+
+
+
 
         // Ajouter un écouteur de toucher à l'imageView
         ball.setOnTouchListener(new View.OnTouchListener() {
+
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
@@ -74,18 +119,47 @@ public class Niveau1 extends AppCompatActivity {
                         ball.setX(ball.getX() + dx);
                         ball.setY(ball.getY() + dy);
 
+                        // point sur balle
+                        ball_t.y = ball.getY() - ball_radius;
+                        ball_b.y = ball.getY() + ball_radius;
+
+
+
                         // Mettre à jour les nouvelles coordonnées de la balle
                         x = event.getX();
                         y = event.getY();
 
 
+                        //Log.d("Y",  "Y : " + ball.getY() );
+                        //Log.d("Y",  "X : " + ball.getX() );
 
-                        // Vérifier si la balle quitte le seau
-                        if (!isViewOverlapping(ball, bucket)) {
-                            // Si la balle quitte le seau, la rendre visible
-                            ball.setVisibility(View.VISIBLE);
-                            //ball.setVisibility(View.INVISIBLE);
+                        Log.d("LEFT",  "X : " + hoop_l.x );
+                        Log.d("LEFT",  "Y : " + hoop_l.y );
+
+                        Log.d("RIGHT",  "X : " + hoop_r.x );
+                        Log.d("RIGHT",  "Y : " + hoop_r.y);
+
+
+
+                        if ( hoop_l.y == Math.round(ball_b.y) && Math.round(ball_b.x) < hoop_r.x && Math.round(ball_b.x) > hoop_l.x ){
+                            Log.d("test1",  "1 est validé : " );
+                            bottom_check = true;
+
                         }
+
+                        if ( hoop_l.y == Math.round(ball_t.y) && Math.round(ball_t.x) < hoop_r.x && Math.round(ball_t.x) > hoop_l.x ){
+                            Log.d("test2",  "2 est validé : " );
+                            top_check = true;
+                        }
+
+
+                        if (top_check && bottom_check){
+                            Log.d("TAG", "Validéeeeeee");
+
+                        }
+
+
+
 
                         break;
                 }
@@ -102,27 +176,6 @@ public class Niveau1 extends AppCompatActivity {
         });
     }
 
-
-
-    private boolean isViewOverlapping(View firstView, View secondView) {
-        int[] firstPosition = new int[2];
-        int[] secondPosition = new int[2];
-
-        firstView.getLocationOnScreen(firstPosition);
-        secondView.getLocationOnScreen(secondPosition);
-
-        int firstViewLeft = firstPosition[0];
-        int firstViewTop = firstPosition[1];
-        int firstViewRight = firstViewLeft + firstView.getWidth();
-        int firstViewBottom = firstViewTop + firstView.getHeight();
-
-        int secondViewLeft = secondPosition[0];
-        int secondViewTop = secondPosition[1];
-        int secondViewRight = secondViewLeft + secondView.getWidth();
-        int secondViewBottom = secondViewTop + secondView.getHeight();
-
-        return !(firstViewLeft > secondViewRight || firstViewRight < secondViewLeft || firstViewTop > secondViewBottom || firstViewBottom < secondViewTop);
-    }
 
     public void goBack(View view){
         Intent intent = new Intent(this, Difficultes.class);
